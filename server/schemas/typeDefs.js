c16b6e5c707bc4ef86f1fc83b2945711f9a59fdc
JRob <72467031+Jonathan-84@users.noreply.github.com>
2021-04-06 19:36:29 -0400

    initial login set up

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,29 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+type Query {
+    me: User
+}
+type User {
+    _id: ID
+    username: String
+    email: String!
+    savedMovies: [Movie]
+    movieCount: Int
+}
+
+type Auth {
+    token: ID!
+    user: User
+  }
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+}
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
