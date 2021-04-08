e715b9c1543f115f78f2663696c847826b836837
MCannon33 <marialcannon@gmail.com>
2021-04-08 19:39:51 -0400

    base typedefs and resolvers

---
+++ server/models/User.js
@@ -0,0 +1,57 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+// import schema
+// const movieSchema = require('./Movie');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Must use a valid email address"],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    // set savedMovie
+    savedMovie: [movieSchema],
+  },
+  // set this to use virtual below
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+//user password
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// validate password
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+//Movie Count
+userSchema.virtual("movieCount").get(function () {
+  return this.savedMovie.length;
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,3 +1,40 @@
 const { qql } = require("apollo-server-express");
 
-// create our typeDefs
+// create typeDefs
+const typeDefs = gql`
+  type Movie {
+    movie_id: Int
+    title: String
+    overview: [String]
+  }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    movieCount: Int
+    savedMovie: [Movie]
+  }
+  input savedMovie {
+    movie_id: Int
+    title: String
+    overview: [String]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    savedMovie(input: savedMovie!): User
+    removeMovie(movie_id: ID!): User
+  }
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
