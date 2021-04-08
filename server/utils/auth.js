862c55deb73742a25565cf3d7d1f6fd14ed275a4
MCannon33 <marialcannon@gmail.com>
2021-04-08 19:44:37 -0400

    update auth

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 // set token secret and expiration
-const secret = "mysecretsshhhhh";
-const expiration = "2h";
+const secret = "mysecret";
+const expiration = "9h";
 
 module.exports = {
   signToken: function ({ username, email, _id }) {
