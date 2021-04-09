e4940edbba28e189abe398f1d7ffc4dddb5f10df
kesiahp18 <kesiahp18@gmail.com>
2021-04-08 20:06:55 -0400

    nav searchbar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -14,10 +14,15 @@ function Nav(props) {
                 <ul className="navbar-nav ml-auto">
                     {tabs.map (tab => (
                     <li key ={tab} className="nav-item">
-                        <a className="nav-link" href={`#${tab.toLowerCase()}`} onClick={() => props.handlePageChange(tab)}>{tab}<span className="sr-only">(current)</span></a>
+                        <a className="nav-link orange-button" href={`#${tab.toLowerCase()}`} onClick={() => props.handlePageChange(tab)}>{tab}<span className="sr-only">(current)</span></a>
                     </li>
                     ))}
                 </ul>
+                <div class="input-group">
+                <input type="search" class="form-control rounded searchbar" placeholder="Search for movies..." aria-label="Search"
+                    aria-describedby="search-addon" />
+                <button type="button" class="btn btn-outline-primary orange-button">Search</button>
+                </div>
             </div>
         </div>
     </nav>
