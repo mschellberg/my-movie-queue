6d29056a1fc90ab6f3a6e7be322fd446cdd6baba
MCannon33 <marialcannon@gmail.com>
2021-04-11 11:31:44 -0400

    update json and controllers

---
+++ server/routes/user-routes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router;
+const {
+  createUser,
+  getOneUser,
+  savedMovie,
+  deleteMovie,
+  login,
+} = require("../../controllers/user-controller");
+
+// import middleware
+const { authMiddleware } = require("../../utils/auth");
+
+// send a token for verification of user
+router.route("/").post(createUser).put(authMiddleware, savedMovie);
+
+router.route("/login").post(login);
+
+router.route("/me").get(authMiddleware, getOneUser);
+
+router.route("/movie/:movie_id").delete(authMiddleware, deleteMovie);
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
+
+const path = require("path");
+const db = require("./config/connection");
+const routes = require("./routes");
+
+const { typeDefs, resolvers } = require("./schemas");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+console.log(process.env.NODE_ENV);
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+app.use(routes);
+
+db.once("open", () => {
+  app.listen(PORT, () =>
+    console.log(`Listening on localhost:${PORT}${server.graphqlPath}`)
+  );
+});
