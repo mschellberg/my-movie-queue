fba6da3d55ec9b5b3c490a33ea30549a4b5880c1
JRob <72467031+Jonathan-84@users.noreply.github.com>
2021-04-16 09:42:20 -0400

    added conditional save buttons

--- client/src/components/MovieDisplay.js
+++ client/src/components/MovieDisplay.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Auth from '../utils/auth';
-import { Button } from 'react-bootstrap';
+//import { Button } from 'react-bootstrap';
 
 class MovieDisplay extends Component {
     watchTrailer() {
@@ -22,13 +22,23 @@ class MovieDisplay extends Component {
                 <p className="description">{this.props.movie.overview}</p>
                 </td>
                 {Auth.loggedIn() ? (
-                <>
+                    <div className="dropdown">
+                    <button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Save to List
+                    <span className="caret"></span></button>
+                    <ul className="dropdown-menu">
+                      <li><a href="#">Save to Watchlist</a></li>
+                      <li><a href="#">Save to Favorites</a></li>
+                     {/*} <li><a href="#">Save to My Movie Shelf</a></li>{*/}
+                    </ul>
+                    <input className="btn btn-primary" type="playTrailer" onClick={this.watchTrailer.bind(this)} value="Play trailer"/>
+                  </div>
+               /*} <>
                   <Button className='btn-block btn-info'> Save to Watchlist</Button>
                   <Button className='btn-block btn-info'> Save to Favorites</Button>
                   <input type="playTrailer" onClick={this.watchTrailer.bind(this)} value="Play trailer"/>
-                </>
+                </>{*/
               ) : (
-                <input type="playTrailer" onClick={this.watchTrailer.bind(this)} value="Play trailer"/>
+                <input className="btn btn-primary" type="playTrailer" onClick={this.watchTrailer.bind(this)} value="Play trailer"/>
               )}
                 </tr>
         </tbody>
