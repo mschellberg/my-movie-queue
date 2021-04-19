b550ca01c8a47528f84175f9ff20a407f1f6c7ec
JRob <72467031+Jonathan-84@users.noreply.github.com>
2021-04-18 22:35:09 -0400

    changed app.js and client json- fix network issue

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,12 @@
-const router = require("express").Router();
-const userRoutes = require("./user-routes");
+const router = require('express').Router();
+const path = require('path');
+const apiRoutes = require('./api');
 
-router.use("/users", userRoutes);
+router.use('/api', apiRoutes);
+
+// serve up react front-end in production
+router.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+});
 
 module.exports = router;
